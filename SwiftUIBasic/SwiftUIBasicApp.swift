38ce2cac9786ffa2d8752bce486fd415ab044f66
tunastorm <ycw921@naver.com>
2024-08-21 09:43:53 +0900

    Initial Commit

---
+++ SwiftUIBasic/SwiftUIBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicApp.swift
+//  SwiftUIBasic
+//
+//  Created by 유철원 on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
